bec316601e7303248fa1266a7d0b24c6e996ae94
Andres Parra <byandrev@gmail.com>
2023-07-14 15:27:02 -0500

    add response type to auth service

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -1,19 +1,24 @@
+import ResponseAPI from "../@types/ReponseAPI";
 import { API_URL } from "../config";
 
-async function login(email: string, password: string): Promise<any> {
-  const response = await fetch(`${API_URL}/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, password }),
-  });
-  const data = response.json();
+async function login(email: string, password: string): Promise<ResponseAPI> {
+  try {
+    const response = await fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = response.json();
 
-  if (response.ok) {
-    return data;
-  } else {
-    throw new Error("Bad credentials");
+    if (response.ok) {
+      return data as Promise<ResponseAPI>;
+    } else {
+      throw new Error("Bad credentials");
+    }
+  } catch (error) {
+    throw new Error("Bad newtwork error");
   }
 }
 
@@ -21,20 +26,24 @@ async function register(
   email: string,
   name: string,
   password: string
-): Promise<any> {
-  const response = await fetch(`${API_URL}/register`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, name, password }),
-  });
-  const data = response.json();
+): Promise<ResponseAPI> {
+  try {
+    const response = await fetch(`${API_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, name, password }),
+    });
+    const data = response.json();
 
-  if (response.ok) {
-    return data;
-  } else {
-    throw new Error("Sign up failed");
+    if (response.ok) {
+      return data as Promise<ResponseAPI>;
+    } else {
+      throw new Error("Sign up failed");
+    }
+  } catch (error) {
+    throw new Error("Bad newtwork error");
   }
 }
 
